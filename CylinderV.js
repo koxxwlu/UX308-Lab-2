ee9652a1df9add7221a436aa803b138b3b7c5d2b
koxxwlu <144965789+koxxwlu@users.noreply.github.com>
2025-01-17 01:34:48 +0000

    Add console statement

--- CylinderV.js
+++ CylinderV.js
@@ -1,8 +1,7 @@
 let diameter = 10;
 let height = 20;
-let pi = Math.pi;
+let pi = 22/7;
 
 volume = (diameter/2)**2 * pi * height;
 
-console.log(volume);
-// console.log(`The volume for a cylinder with ${volume} for the .`);
+console.log(`The volume for a cylinder with height of ${height} CM & diameter of ${diameter} has a volume of ${volume} CM2.`);
